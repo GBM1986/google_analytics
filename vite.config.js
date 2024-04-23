54e357d2d9226419987a7cff63c324eb66875b14
GBM1986 <gunnar_bjarni@hotmail.com>
2024-04-23 14:23:32 +0200

    asdas

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.JPG"],
-  base: "/dist/",
-});
+  base: 'https://gbm1986.github.io/google_analytics/',
+})
