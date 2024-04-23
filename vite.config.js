fcd784cada4e771475a356f5eaaa03ffb6da9838
GBM1986 <gunnar_bjarni@hotmail.com>
2024-04-23 14:14:37 +0200

    asda

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist/',
   plugins: [react()],
-  base: 'https://gbm1986.github.io/google_analytics/',
 })
