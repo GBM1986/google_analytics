3d84ca170b133145a2ae274925bc35fbdcdfe2a9
GBM1986 <gunnar_bjarni@hotmail.com>
2024-04-23 14:31:23 +0200

    dsfds

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { About } from "../pages/About";
 import { Home } from "../pages/Home";
 import { Gallery } from "../pages/Gallery";
 
 
-export const routes = createBrowserRouter([
+export const routes = createHashRouter([
     {
         path: "/",
         element: <Layout />,
